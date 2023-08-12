fe114bf1bdbf921a2252a589158b056477319f27
Dawidu <dawidtomalik7@gmail.com>
2023-08-12 16:44:12 +0200

    NextAuth config file change

---
+++ src/app/api/auth/[...nextauth]/config.ts
@@ -0,0 +1,36 @@
+import { compare } from "bcrypt"
+import type { NextAuthOptions } from "next-auth"
+import Credentials from "next-auth/providers/credentials"
+import db from "~/db"
+
+export const authOptions: NextAuthOptions = {
+  pages: {
+    signIn: "/login",
+  },
+  providers: [
+    Credentials({
+      credentials: {
+        password: { type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.password) return null
+
+        const admin = await db.query.admin.findFirst()
+
+        if (!admin) return null
+
+        const isPasswordValid = await compare(
+          credentials.password,
+          admin.password,
+        )
+
+        if (!isPasswordValid) return null
+
+        return { id: admin.id.toString() }
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+}
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,40 +1,5 @@
-import { compare } from "bcrypt"
 import NextAuth from "next-auth"
-import type { NextAuthOptions } from "next-auth"
-import Credentials from "next-auth/providers/credentials"
-import db from "~/db"
-
-export const authOptions: NextAuthOptions = {
-  pages: {
-    signIn: "/login",
-  },
-  providers: [
-    Credentials({
-      credentials: {
-        password: { type: "password" },
-      },
-      async authorize(credentials) {
-        if (!credentials?.password) return null
-
-        const admin = await db.query.admin.findFirst()
-
-        if (!admin) return null
-
-        const isPasswordValid = await compare(
-          credentials.password,
-          admin.password,
-        )
-
-        if (!isPasswordValid) return null
-
-        return { id: admin.id.toString() }
-      },
-    }),
-  ],
-  session: {
-    strategy: "jwt",
-  },
-}
+import { authOptions } from "./config"
 
 const handler = NextAuth(authOptions)
 export { handler as GET, handler as POST }
